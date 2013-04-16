47a8ee39d4e77e5d9ec0666c33c1066b951bfb52
koji-- <kojiel@gmail.com>
2013-04-17 03:08:01 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::LightNovel - It's new $module
+
+# SYNOPSIS
+
+    use Acme::LightNovel;
+
+# DESCRIPTION
+
+Acme::LightNovel is ...
+
+# LICENSE
+
+Copyright (C) koji--
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+koji-- <kojiel@gmail.com>
---
+++ lib/Acme/LightNovel.pm
@@ -0,0 +1,36 @@
+package Acme::LightNovel;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::LightNovel - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::LightNovel;
+
+=head1 DESCRIPTION
+
+Acme::LightNovel is ...
+
+=head1 LICENSE
+
+Copyright (C) koji--
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+koji-- E<lt>kojiel@gmail.comE<gt>
+
